7aac0bf42769877da76b886bb647c3c4f84c1c2e
fahxalas <141108120+fahxalas@users.noreply.github.com>
2026-07-28 02:32:00 -0500

    Refactor display and button handling in xingzhi cube
    Refactor display handling and button functionality. Replace volume buttons with boot button functionality and update power manager initialization.

--- main/boards/nologo/xingzhi-cube-1.54tft-wifi/xingzhi-cube-1.54tft-wifi.cc
+++ main/boards/nologo/xingzhi-cube-1.54tft-wifi/xingzhi-cube-1.54tft-wifi.cc
@@ -6,32 +6,55 @@
 #include "button.h"
 #include "config.h"
 #include "power_save_timer.h"
-#include "led/single_led.h"
 #include "assets/lang_config.h"
 #include "power_manager.h"
 
 #include <esp_log.h>
 #include <esp_lcd_panel_vendor.h>
-#include <esp_lcd_nv3023.h> // Driver NV3023
-
-#include <driver/rtc_io.h>
-#include <esp_sleep.h>
+#include <esp_lcd_nv3023.h>
 
 #define TAG "XINGZHI_CUBE_1_54TFT_WIFI"
+
+class NV3023Display : public SpiLcdDisplay {
+public:
+    NV3023Display(esp_lcd_panel_io_handle_t panel_io, esp_lcd_panel_handle_t panel,
+                int width, int height, int offset_x, int offset_y, bool mirror_x, bool mirror_y, bool swap_xy)
+        : SpiLcdDisplay(panel_io, panel, width, height, offset_x, offset_y, mirror_x, mirror_y, swap_xy) {
+    }
+
+    void SetupUI() override {
+        SpiLcdDisplay::SetupUI();
+
+        DisplayLockGuard lock(this);
+        auto screen = lv_disp_get_scr_act(lv_disp_get_default());
+        lv_obj_set_style_text_color(screen, lv_color_black(), 0);
+        lv_obj_set_style_bg_color(container_, lv_color_black(), 0);
+
+        lv_obj_set_style_bg_color(status_bar_, lv_color_white(), 0);
+        lv_obj_set_style_text_color(network_label_, lv_color_black(), 0);
+        lv_obj_set_style_text_color(notification_label_, lv_color_black(), 0);
+        lv_obj_set_style_text_color(status_label_, lv_color_black(), 0);
+        lv_obj_set_style_text_color(mute_label_, lv_color_black(), 0);
+        lv_obj_set_style_text_color(battery_label_, lv_color_black(), 0);
+
+        lv_obj_set_style_bg_color(content_, lv_color_black(), 0);
+        lv_obj_set_style_border_width(content_, 0, 0);
+        lv_obj_set_style_text_color(emoji_label_, lv_color_white(), 0);
+        lv_obj_set_style_text_color(chat_message_label_, lv_color_white(), 0);
+    }
+};
 
 class XINGZHI_CUBE_1_54TFT_WIFI : public WifiBoard {
 private:
     Button boot_button_;
-    Button volume_up_button_;
-    Button volume_down_button_;
-    SpiLcdDisplay* display_;
+    NV3023Display* display_;
     PowerSaveTimer* power_save_timer_;
     PowerManager* power_manager_;
     esp_lcd_panel_io_handle_t panel_io_ = nullptr;
     esp_lcd_panel_handle_t panel_ = nullptr;
 
     void InitializePowerManager() {
-        power_manager_ = new PowerManager(GPIO_NUM_38);
+        power_manager_ = new PowerManager(GPIO_NUM_48);
         power_manager_->OnChargingStatusChanged([this](bool is_charging) {
             if (is_charging) {
                 power_save_timer_->SetEnabled(false);
@@ -42,11 +65,7 @@ private:
     }
 
     void InitializePowerSaveTimer() {
-        rtc_gpio_init(GPIO_NUM_21);
-        rtc_gpio_set_direction(GPIO_NUM_21, RTC_GPIO_MODE_OUTPUT_ONLY);
-        rtc_gpio_set_level(GPIO_NUM_21, 1);
-
-        power_save_timer_ = new PowerSaveTimer(-1, 60, 300);
+        power_save_timer_ = new PowerSaveTimer(240, 60, -1);
         power_save_timer_->OnEnterSleepMode([this]() {
             GetDisplay()->SetPowerSaveMode(true);
             GetBacklight()->SetBrightness(1);
@@ -54,13 +73,6 @@ private:
         power_save_timer_->OnExitSleepMode([this]() {
             GetDisplay()->SetPowerSaveMode(false);
             GetBacklight()->RestoreBrightness();
-        });
-        power_save_timer_->OnShutdownRequest([this]() {
-            ESP_LOGI(TAG, "Shutting down");
-            rtc_gpio_set_level(GPIO_NUM_21, 0);
-            rtc_gpio_hold_en(GPIO_NUM_21);
-            esp_lcd_panel_disp_on_off(panel_, false);
-            esp_deep_sleep_start();
         });
         power_save_timer_->SetEnabled(true);
     }
@@ -86,39 +98,12 @@ private:
             }
             app.ToggleChatState();
         });
-
-        volume_up_button_.OnClick([this]() {
+        boot_button_.OnPressDown([this]() {
             power_save_timer_->WakeUp();
-            auto codec = GetAudioCodec();
-            auto volume = codec->output_volume() + 10;
-            if (volume > 100) {
-                volume = 100;
-            }
-            codec->SetOutputVolume(volume);
-            GetDisplay()->ShowNotification(Lang::Strings::VOLUME + std::to_string(volume));
+            Application::GetInstance().StartListening();
         });
-
-        volume_up_button_.OnLongPress([this]() {
-            power_save_timer_->WakeUp();
-            GetAudioCodec()->SetOutputVolume(100);
-            GetDisplay()->ShowNotification(Lang::Strings::MAX_VOLUME);
-        });
-
-        volume_down_button_.OnClick([this]() {
-            power_save_timer_->WakeUp();
-            auto codec = GetAudioCodec();
-            auto volume = codec->output_volume() - 10;
-            if (volume < 0) {
-                volume = 0;
-            }
-            codec->SetOutputVolume(volume);
-            GetDisplay()->ShowNotification(Lang::Strings::VOLUME + std::to_string(volume));
-        });
-
-        volume_down_button_.OnLongPress([this]() {
-            power_save_timer_->WakeUp();
-            GetAudioCodec()->SetOutputVolume(0);
-            GetDisplay()->ShowNotification(Lang::Strings::MUTED);
+        boot_button_.OnPressUp([this]() {
+            Application::GetInstance().StopListening();
         });
     }
 
@@ -148,15 +133,13 @@ private:
         esp_lcd_panel_mirror(panel_, DISPLAY_MIRROR_X, DISPLAY_MIRROR_Y);
         ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_, true));
 
-        display_ = new SpiLcdDisplay(panel_io_, panel_, DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_OFFSET_X, DISPLAY_OFFSET_Y, 
+        display_ = new NV3023Display(panel_io_, panel_, DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_OFFSET_X, DISPLAY_OFFSET_Y, 
             DISPLAY_MIRROR_X, DISPLAY_MIRROR_Y, DISPLAY_SWAP_XY);
     }
 
 public:
     XINGZHI_CUBE_1_54TFT_WIFI() :
-        boot_button_(BOOT_BUTTON_GPIO),
-        volume_up_button_(VOLUME_UP_BUTTON_GPIO),
-        volume_down_button_(VOLUME_DOWN_BUTTON_GPIO) {
+        boot_button_(BOOT_BUTTON_GPIO) {
         InitializePowerManager();
         InitializePowerSaveTimer();
         InitializeSpi();
