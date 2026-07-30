32138142b30e09224cedf6495f3672818f6bb1c4
fahxalas <141108120+fahxalas@users.noreply.github.com>
2026-07-30 14:15:47 -0500

    Refactor magiclick_2p4_board class structure and dependencies

--- main/boards/magiclick/2p4/magiclick_2p4_board.cc
+++ main/boards/magiclick/2p4/magiclick_2p4_board.cc
@@ -1,75 +1,26 @@
 #include "wifi_board.h"
+#include "codecs/es8311_audio_codec.h"
 #include "display/lcd_display.h"
-#include "codecs/no_audio_codec.h"
 #include "application.h"
 #include "button.h"
-#include "led/circular_strip.h"
 #include "config.h"
-#include "assets/lang_config.h"
-
-#include <esp_lcd_panel_vendor.h>
-#include <esp_log.h>
-#include <driver/spi_common.h>
-#include <esp_lcd_nv3023.h>
-
-#include "../2p5/power_manager.h"
 #include "power_save_timer.h"
 
-#define TAG "magiclick_2p4"
+#include <esp_log.h>
+#include <esp_lcd_panel_vendor.h>
+#include <esp_lcd_nv3023.h>
+#include <driver/gpio.h>
+#include <driver/i2c.h>
 
-class NV3023Display : public SpiLcdDisplay {
-public:
-    NV3023Display(esp_lcd_panel_io_handle_t panel_io, esp_lcd_panel_handle_t panel,
-                int width, int height, int offset_x, int offset_y, bool mirror_x, bool mirror_y, bool swap_xy)
-        : SpiLcdDisplay(panel_io, panel, width, height, offset_x, offset_y, mirror_x, mirror_y, swap_xy) {
-    }
+#define TAG "MAGICLICK_2P4"
 
-    void SetupUI() override {
-        SpiLcdDisplay::SetupUI();
-
-        DisplayLockGuard lock(this);
-        auto screen = lv_disp_get_scr_act(lv_disp_get_default());
-        lv_obj_set_style_text_color(screen, lv_color_black(), 0);
-
-        lv_obj_set_style_bg_color(container_, lv_color_black(), 0);
-
-        lv_obj_set_style_bg_color(status_bar_, lv_color_white(), 0);
-        lv_obj_set_style_text_color(network_label_, lv_color_black(), 0);
-        lv_obj_set_style_text_color(notification_label_, lv_color_black(), 0);
-        lv_obj_set_style_text_color(status_label_, lv_color_black(), 0);
-        lv_obj_set_style_text_color(mute_label_, lv_color_black(), 0);
-        lv_obj_set_style_text_color(battery_label_, lv_color_black(), 0);
-
-        lv_obj_set_style_bg_color(content_, lv_color_black(), 0);
-        lv_obj_set_style_border_width(content_, 0, 0);
-        lv_obj_set_style_text_color(emoji_label_, lv_color_white(), 0);
-        lv_obj_set_style_text_color(chat_message_label_, lv_color_white(), 0);
-    }
-};
-
-class magiclick_2p4 : public WifiBoard {
+class MagiClick2P4Board : public WifiBoard {
 private:
-    Button main_button_;
-    Button left_button_;
-    Button right_button_;
-    NV3023Display* display_;
-
-    PowerSaveTimer* power_save_timer_;
-    PowerManager* power_manager_;
-
-    esp_lcd_panel_io_handle_t panel_io = nullptr;
-    esp_lcd_panel_handle_t panel = nullptr;
-
-    void InitializePowerManager() {
-        power_manager_ = new PowerManager(GPIO_NUM_48);
-        power_manager_->OnChargingStatusChanged([this](bool is_charging) {
-            if (is_charging) {
-                power_save_timer_->SetEnabled(false);
-            } else {
-                power_save_timer_->SetEnabled(true);
-            }
-        });
-    }
+    Button boot_button_;
+    SpiLcdDisplay* display_ = nullptr;
+    PowerSaveTimer* power_save_timer_ = nullptr;
+    esp_lcd_panel_io_handle_t panel_io_ = nullptr;
+    esp_lcd_panel_handle_t panel_ = nullptr;
 
     void InitializePowerSaveTimer() {
         power_save_timer_ = new PowerSaveTimer(240, 60, -1);
@@ -81,135 +32,96 @@ private:
             GetDisplay()->SetPowerSaveMode(false);
             GetBacklight()->RestoreBrightness();
         });
-         
         power_save_timer_->SetEnabled(true);
     }
 
-    void InitializeButtons() {
-        main_button_.OnClick([this]() {
-            auto& app = Application::GetInstance();
-            if (app.GetDeviceState() == kDeviceStateStarting) {
-                EnterWifiConfigMode();
-                return;
-            }
-        });
-        main_button_.OnPressDown([this]() {
-            power_save_timer_->WakeUp();
-            Application::GetInstance().StartListening();
-        });
-        main_button_.OnPressUp([this]() {
-            Application::GetInstance().StopListening();
-        });
-
-        left_button_.OnClick([this]() {
-            power_save_timer_->WakeUp();
-            auto& app = Application::GetInstance();
-            if (app.GetDeviceState() == kDeviceStateStarting) {
-                EnterWifiConfigMode();
-                return;
-            }
-            auto codec = GetAudioCodec();
-            auto volume = codec->output_volume() - 10;
-            if (volume < 0) {
-                volume = 0;
-            }
-            codec->SetOutputVolume(volume);
-            GetDisplay()->ShowNotification(Lang::Strings::VOLUME + std::to_string(volume));
-        });
-
-        left_button_.OnLongPress([this]() {
-            power_save_timer_->WakeUp();
-            GetAudioCodec()->SetOutputVolume(0);
-            GetDisplay()->ShowNotification(Lang::Strings::MUTED);
-        });
-
-        right_button_.OnClick([this]() {
-            power_save_timer_->WakeUp();
-            auto codec = GetAudioCodec();
-            auto volume = codec->output_volume() + 10;
-            if (volume > 100) {
-                volume = 100;
-            }
-            codec->SetOutputVolume(volume);
-            GetDisplay()->ShowNotification(Lang::Strings::VOLUME + std::to_string(volume));
-        });
-
-        right_button_.OnLongPress([this]() {
-            power_save_timer_->WakeUp();
-            GetAudioCodec()->SetOutputVolume(100);
-            GetDisplay()->ShowNotification(Lang::Strings::MAX_VOLUME);
-        });
-    }
-
-    void InitializeLedPower() {
-        gpio_reset_pin(BUILTIN_LED_POWER);
-        gpio_set_direction(BUILTIN_LED_POWER, GPIO_MODE_OUTPUT);
-        gpio_set_level(BUILTIN_LED_POWER, BUILTIN_LED_POWER_OUTPUT_INVERT ? 0 : 1);
+    void InitializeI2c() {
+        i2c_config_t conf = {};
+        conf.mode = I2C_MODE_MASTER;
+        conf.sda_io_num = AUDIO_CODEC_I2C_SDA_PIN;
+        conf.scl_io_num = AUDIO_CODEC_I2C_SCL_PIN;
+        conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+        conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+        conf.master.clk_speed = 100000;
+        ESP_ERROR_CHECK(i2c_param_config(I2C_NUM_0, &conf));
+        ESP_ERROR_CHECK(i2c_driver_install(I2C_NUM_0, conf.mode, 0, 0, 0));
     }
 
     void InitializeSpi() {
         spi_bus_config_t buscfg = {};
-        buscfg.mosi_io_num = DISPLAY_SDA_PIN;
+        buscfg.mosi_io_num = DISPLAY_SDA;
         buscfg.miso_io_num = GPIO_NUM_NC;
-        buscfg.sclk_io_num = DISPLAY_SCL_PIN;
+        buscfg.sclk_io_num = DISPLAY_SCL;
         buscfg.quadwp_io_num = GPIO_NUM_NC;
         buscfg.quadhd_io_num = GPIO_NUM_NC;
         buscfg.max_transfer_sz = DISPLAY_WIDTH * DISPLAY_HEIGHT * sizeof(uint16_t);
         ESP_ERROR_CHECK(spi_bus_initialize(SPI3_HOST, &buscfg, SPI_DMA_CH_AUTO));
     }
 
-    void InitializeNv3023Display(){
+    void InitializeButtons() {
+        boot_button_.OnClick([this]() {
+            power_save_timer_->WakeUp();
+            auto& app = Application::GetInstance();
+            if (app.GetDeviceState() == kDeviceStateStarting) {
+                EnterWifiConfigMode();
+                return;
+            }
+            app.ToggleChatState();
+        });
+        boot_button_.OnPressDown([this]() {
+            power_save_timer_->WakeUp();
+            Application::GetInstance().StartListening();
+        });
+        boot_button_.OnPressUp([this]() {
+            Application::GetInstance().StopListening();
+        });
+    }
+
+    void InitializeNv3023Display() {
         ESP_LOGD(TAG, "Install panel IO");
         esp_lcd_panel_io_spi_config_t io_config = {};
-        io_config.cs_gpio_num = DISPLAY_CS_PIN;
-        io_config.dc_gpio_num = DISPLAY_DC_PIN;
+        io_config.cs_gpio_num = DISPLAY_CS;
+        io_config.dc_gpio_num = DISPLAY_DC;
         io_config.spi_mode = 0;
         io_config.pclk_hz = 40 * 1000 * 1000;
         io_config.trans_queue_depth = 10;
         io_config.lcd_cmd_bits = 8;
         io_config.lcd_param_bits = 8;
-        ESP_ERROR_CHECK(esp_lcd_new_panel_io_spi(SPI3_HOST, &io_config, &panel_io));
+        ESP_ERROR_CHECK(esp_lcd_new_panel_io_spi(SPI3_HOST, &io_config, &panel_io_));
 
-        ESP_LOGD(TAG, "Install LCD driver");
+        ESP_LOGD(TAG, "Install LCD driver NV3023");
         esp_lcd_panel_dev_config_t panel_config = {};
-        panel_config.reset_gpio_num = DISPLAY_RST_PIN;
+        panel_config.reset_gpio_num = DISPLAY_RES;
         panel_config.rgb_ele_order = LCD_RGB_ELEMENT_ORDER_BGR;
         panel_config.bits_per_pixel = 16;
-        ESP_ERROR_CHECK(esp_lcd_new_panel_nv3023(panel_io, &panel_config, &panel));
+        ESP_ERROR_CHECK(esp_lcd_new_panel_nv3023(panel_io_, &panel_config, &panel_));
 
-        esp_lcd_panel_reset(panel);
+        esp_lcd_panel_reset(panel_);
+        esp_lcd_panel_init(panel_);
+        esp_lcd_panel_invert_color(panel_, false);
+        esp_lcd_panel_swap_xy(panel_, DISPLAY_SWAP_XY);
+        esp_lcd_panel_mirror(panel_, DISPLAY_MIRROR_X, DISPLAY_MIRROR_Y);
+        ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_, true));
 
-        esp_lcd_panel_init(panel);
-        esp_lcd_panel_invert_color(panel, false);
-        esp_lcd_panel_swap_xy(panel, DISPLAY_SWAP_XY);
-        esp_lcd_panel_mirror(panel, DISPLAY_MIRROR_X, DISPLAY_MIRROR_Y);
-        ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel, true));
-        display_ = new NV3023Display(panel_io, panel,
-                                    DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_OFFSET_X, DISPLAY_OFFSET_Y, DISPLAY_MIRROR_X, DISPLAY_MIRROR_Y, DISPLAY_SWAP_XY);
+        // Usa SpiLcdDisplay directamente para evitar accesos a memoria nula en LVGL
+        display_ = new SpiLcdDisplay(panel_io_, panel_, DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_OFFSET_X, DISPLAY_OFFSET_Y, 
+            DISPLAY_MIRROR_X, DISPLAY_MIRROR_Y, DISPLAY_SWAP_XY);
     }
 
 public:
-    magiclick_2p4() :
-        main_button_(MAIN_BUTTON_GPIO),
-        left_button_(LEFT_BUTTON_GPIO), 
-        right_button_(RIGHT_BUTTON_GPIO) {
-        InitializeLedPower();
-        InitializePowerManager();
-        InitializePowerSaveTimer();        
-        InitializeButtons();
+    MagiClick2P4Board() : boot_button_(BOOT_BUTTON_GPIO) {
+        InitializeI2c();
+        InitializePowerSaveTimer();
         InitializeSpi();
+        InitializeButtons();
         InitializeNv3023Display();
         GetBacklight()->RestoreBrightness();
     }
 
-    virtual Led* GetLed() override {
-        static CircularStrip led(BUILTIN_LED_GPIO, BUILTIN_LED_NUM);
-        return &led;
-    }
-
     virtual AudioCodec* GetAudioCodec() override {
-        static NoAudioCodecSimplex audio_codec(AUDIO_INPUT_SAMPLE_RATE, AUDIO_OUTPUT_SAMPLE_RATE,
-            AUDIO_I2S_GPIO_BCLK, AUDIO_I2S_GPIO_WS, AUDIO_I2S_GPIO_DOUT, AUDIO_I2S_GPIO_BCLK, AUDIO_I2S_GPIO_WS, AUDIO_I2S_GPIO_DIN);
+        static Es8311AudioCodec audio_codec(I2C_NUM_0, AUDIO_CODEC_I2C_ADDR, AUDIO_INPUT_SAMPLE_RATE, AUDIO_OUTPUT_SAMPLE_RATE,
+            AUDIO_I2S_SPK_GPIO_BCLK, AUDIO_I2S_SPK_GPIO_LRCK, AUDIO_I2S_SPK_GPIO_DOUT, AUDIO_I2S_MIC_GPIO_SCK, AUDIO_I2S_MIC_GPIO_WS, AUDIO_I2S_MIC_GPIO_DIN,
+            AUDIO_CODEC_PA_PIN, AUDIO_CODEC_ES8311_MCLK_PIN);
         return &audio_codec;
     }
 
@@ -222,18 +134,6 @@ public:
         return &backlight;
     }
 
-    virtual bool GetBatteryLevel(int& level, bool& charging, bool& discharging) override {
-        static bool last_discharging = false;
-        charging = power_manager_->IsCharging();
-        discharging = power_manager_->IsDischarging();
-        if (discharging != last_discharging) {
-            power_save_timer_->SetEnabled(discharging);
-            last_discharging = discharging;
-        }
-        level = power_manager_->GetBatteryLevel();
-        return true;
-    }
-
     virtual void SetPowerSaveLevel(PowerSaveLevel level) override {
         if (level != PowerSaveLevel::LOW_POWER) {
             power_save_timer_->WakeUp();
@@ -242,4 +142,4 @@ public:
     }
 };
 
-DECLARE_BOARD(magiclick_2p4);
+DECLARE_BOARD(MagiClick2P4Board);
