b7270ce0536c988ae3b57589ac3610007f458010
fahxalas <141108120+fahxalas@users.noreply.github.com>
2026-07-30 14:28:23 -0500

    Refactor display handling and I2C initialization

--- main/boards/magiclick/2p4/magiclick_2p4_board.cc
+++ main/boards/magiclick/2p4/magiclick_2p4_board.cc
@@ -5,19 +5,71 @@
 #include "button.h"
 #include "config.h"
 #include "power_save_timer.h"
+#include "i2c_device.h"
 
 #include <esp_log.h>
 #include <esp_lcd_panel_vendor.h>
 #include <esp_lcd_nv3023.h>
 #include <driver/gpio.h>
-#include <driver/i2c.h>
 
-#define TAG "MAGICLICK_2P4"
+#define TAG "MagiClick2P4Board"
+
+class NV3023Display : public SpiLcdDisplay {
+public:
+    NV3023Display(esp_lcd_panel_io_handle_t panel_io, esp_lcd_panel_handle_t panel,
+                int width, int height, int offset_x, int offset_y, bool mirror_x, bool mirror_y, bool swap_xy)
+        : SpiLcdDisplay(panel_io, panel, width, height, offset_x, offset_y, mirror_x, mirror_y, swap_xy) {
+    }
+
+    void SetupUI() override {
+        SpiLcdDisplay::SetupUI();
+
+        // Se remueve DisplayLockGuard para evitar el colapso de memoria al arrancar LVGL
+        auto screen = lv_disp_get_scr_act(lv_disp_get_default());
+        if (screen != nullptr) {
+            lv_obj_set_style_text_color(screen, lv_color_black(), 0);
+        }
+        if (container_ != nullptr) {
+            lv_obj_set_style_bg_color(container_, lv_color_black(), 0);
+        }
+        if (status_bar_ != nullptr) {
+            lv_obj_set_style_bg_color(status_bar_, lv_color_white(), 0);
+        }
+        if (network_label_ != nullptr) {
+            lv_obj_set_style_text_color(network_label_, lv_color_black(), 0);
+        }
+        if (notification_label_ != nullptr) {
+            lv_obj_set_style_text_color(notification_label_, lv_color_black(), 0);
+        }
+        if (status_label_ != nullptr) {
+            lv_obj_set_style_text_color(status_label_, lv_color_black(), 0);
+        }
+        if (mute_label_ != nullptr) {
+            lv_obj_set_style_text_color(mute_label_, lv_color_black(), 0);
+        }
+        if (battery_label_ != nullptr) {
+            lv_obj_set_style_text_color(battery_label_, lv_color_black(), 0);
+        }
+        if (content_ != nullptr) {
+            lv_obj_set_style_bg_color(content_, lv_color_black(), 0);
+            lv_obj_set_style_border_width(content_, 0, 0);
+        }
+        if (emoji_label_ != nullptr) {
+            lv_obj_set_style_text_color(emoji_label_, lv_color_white(), 0);
+        }
+        if (chat_message_label_ != nullptr) {
+            lv_obj_set_style_text_color(chat_message_label_, lv_color_white(), 0);
+        }
+    }
+};
 
 class MagiClick2P4Board : public WifiBoard {
 private:
+    I2cBus i2c_bus_;
     Button boot_button_;
-    SpiLcdDisplay* display_ = nullptr;
+    Button touch_button_;
+    Button volume_up_button_;
+    NV3023Display* display_ = nullptr;
     PowerSaveTimer* power_save_timer_ = nullptr;
     esp_lcd_panel_io_handle_t panel_io_ = nullptr;
     esp_lcd_panel_handle_t panel_ = nullptr;
@@ -33,18 +85,6 @@ private:
             GetBacklight()->RestoreBrightness();
         });
         power_save_timer_->SetEnabled(true);
-    }
-
-    void InitializeI2c() {
-        i2c_config_t conf = {};
-        conf.mode = I2C_MODE_MASTER;
-        conf.sda_io_num = AUDIO_CODEC_I2C_SDA_PIN;
-        conf.scl_io_num = AUDIO_CODEC_I2C_SCL_PIN;
-        conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
-        conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
-        conf.master.clk_speed = 100000;
-        ESP_ERROR_CHECK(i2c_param_config(I2C_NUM_0, &conf));
-        ESP_ERROR_CHECK(i2c_driver_install(I2C_NUM_0, conf.mode, 0, 0, 0));
     }
 
     void InitializeSpi() {
@@ -75,6 +115,24 @@ private:
         boot_button_.OnPressUp([this]() {
             Application::GetInstance().StopListening();
         });
+
+        touch_button_.OnClick([this]() {
+            power_save_timer_->WakeUp();
+            auto& app = Application::GetInstance();
+            if (app.GetDeviceState() == kDeviceStateStarting) {
+                EnterWifiConfigMode();
+                return;
+            }
+            app.ToggleChatState();
+        });
+
+        volume_up_button_.OnClick([this]() {
+            power_save_timer_->WakeUp();
+            auto codec = GetAudioCodec();
+            auto volume = codec->GetOutputVolume() + 10;
+            if (volume > 100) volume = 100;
+            codec->SetOutputVolume(volume);
+        });
     }
 
     void InitializeNv3023Display() {
@@ -103,14 +161,16 @@ private:
         esp_lcd_panel_mirror(panel_, DISPLAY_MIRROR_X, DISPLAY_MIRROR_Y);
         ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_, true));
 
-        // Usa SpiLcdDisplay directamente para evitar accesos a memoria nula en LVGL
-        display_ = new SpiLcdDisplay(panel_io_, panel_, DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_OFFSET_X, DISPLAY_OFFSET_Y, 
+        display_ = new NV3023Display(panel_io_, panel_, DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_OFFSET_X, DISPLAY_OFFSET_Y, 
             DISPLAY_MIRROR_X, DISPLAY_MIRROR_Y, DISPLAY_SWAP_XY);
     }
 
 public:
-    MagiClick2P4Board() : boot_button_(BOOT_BUTTON_GPIO) {
-        InitializeI2c();
+    MagiClick2P4Board() :
+        i2c_bus_(I2C_NUM_0, AUDIO_CODEC_I2C_SDA_PIN, AUDIO_CODEC_I2C_SCL_PIN),
+        boot_button_(BOOT_BUTTON_GPIO),
+        touch_button_(TOUCH_BUTTON_GPIO),
+        volume_up_button_(VOLUME_UP_BUTTON_GPIO) {
         InitializePowerSaveTimer();
         InitializeSpi();
         InitializeButtons();
@@ -119,9 +179,9 @@ public:
     }
 
     virtual AudioCodec* GetAudioCodec() override {
-        static Es8311AudioCodec audio_codec(I2C_NUM_0, AUDIO_CODEC_I2C_ADDR, AUDIO_INPUT_SAMPLE_RATE, AUDIO_OUTPUT_SAMPLE_RATE,
-            AUDIO_I2S_SPK_GPIO_BCLK, AUDIO_I2S_SPK_GPIO_LRCK, AUDIO_I2S_SPK_GPIO_DOUT, AUDIO_I2S_MIC_GPIO_SCK, AUDIO_I2S_MIC_GPIO_WS, AUDIO_I2S_MIC_GPIO_DIN,
-            AUDIO_CODEC_PA_PIN, AUDIO_CODEC_ES8311_MCLK_PIN);
+        static Es8311AudioCodec audio_codec(i2c_bus_, AUDIO_CODEC_ES8311_ADDR, AUDIO_INPUT_SAMPLE_RATE, AUDIO_OUTPUT_SAMPLE_RATE,
+            AUDIO_I2S_GPIO_MCLK, AUDIO_I2S_GPIO_BCLK, AUDIO_I2S_GPIO_LRCK, AUDIO_I2S_GPIO_DOUT, AUDIO_I2S_GPIO_DIN,
+            AUDIO_CODEC_PA_PIN, AUDIO_CODEC_ES8311_ADDR);
         return &audio_codec;
     }
 
